ab007180145f2867d48ecea17236d32dbb8639ee
jiana <767664233@qq.com>
2020-04-19 15:11:34 +0800

    xiuagi

---
+++ src/main/java/com/electronic/dao/mapper/bo/WorkOrder.java
@@ -0,0 +1,105 @@
+package com.electronic.dao.mapper.bo;
+
+import java.util.Date;
+
+public class WorkOrder {
+    private Integer workOrderId;
+
+    private String workOrderCode;
+
+    private String workOrderName;
+
+    private String organizer;
+
+    private String currentNode;
+
+    private Integer nodeCount;
+
+    private Date createTime;
+
+    private Date operateTime;
+
+    private Integer workOrderStatus;
+
+    private byte[] workInfo;
+
+    public Integer getWorkOrderId() {
+        return workOrderId;
+    }
+
+    public void setWorkOrderId(Integer workOrderId) {
+        this.workOrderId = workOrderId;
+    }
+
+    public String getWorkOrderCode() {
+        return workOrderCode;
+    }
+
+    public void setWorkOrderCode(String workOrderCode) {
+        this.workOrderCode = workOrderCode == null ? null : workOrderCode.trim();
+    }
+
+    public String getWorkOrderName() {
+        return workOrderName;
+    }
+
+    public void setWorkOrderName(String workOrderName) {
+        this.workOrderName = workOrderName == null ? null : workOrderName.trim();
+    }
+
+    public String getOrganizer() {
+        return organizer;
+    }
+
+    public void setOrganizer(String organizer) {
+        this.organizer = organizer == null ? null : organizer.trim();
+    }
+
+    public String getCurrentNode() {
+        return currentNode;
+    }
+
+    public void setCurrentNode(String currentNode) {
+        this.currentNode = currentNode == null ? null : currentNode.trim();
+    }
+
+    public Integer getNodeCount() {
+        return nodeCount;
+    }
+
+    public void setNodeCount(Integer nodeCount) {
+        this.nodeCount = nodeCount;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getOperateTime() {
+        return operateTime;
+    }
+
+    public void setOperateTime(Date operateTime) {
+        this.operateTime = operateTime;
+    }
+
+    public Integer getWorkOrderStatus() {
+        return workOrderStatus;
+    }
+
+    public void setWorkOrderStatus(Integer workOrderStatus) {
+        this.workOrderStatus = workOrderStatus;
+    }
+
+    public byte[] getWorkInfo() {
+        return workInfo;
+    }
+
+    public void setWorkInfo(byte[] workInfo) {
+        this.workInfo = workInfo;
+    }
+}
