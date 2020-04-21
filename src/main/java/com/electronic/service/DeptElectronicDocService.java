a80ad23713e81102e4f38f729cb51e411968bca0
jiana <767664233@qq.com>
2020-04-22 00:37:09 +0800

    xiuagi

--- src/main/java/com/electronic/service/DeptElectronicDocService.java
+++ src/main/java/com/electronic/service/DeptElectronicDocService.java
@@ -3,11 +3,8 @@ package com.electronic.service;
 import com.electronic.base.model.BaseResponse;
 import com.electronic.base.model.PageResult;
 import com.electronic.base.model.VO.DeptElectronicDocVO;
-import com.electronic.base.model.VO.ElectronicDocRequest;
 import com.electronic.base.model.VO.SDeptElectronicDocVO;
 import com.electronic.dao.mapper.bo.DeptElectronicDoc;
-import com.electronic.dao.mapper.bo.ElectronicDoc;
-import com.electronic.dao.smapper.bo.SUserElectronicDoc;
 
 public interface DeptElectronicDocService {
 
